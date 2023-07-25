d72ab3a2e78c2618f293154e0eaad9c312c54045
muhtar-sgk <muhtarrn@gmail.com>
2023-07-25 17:06:27 +0700

    add project

---
+++ android/app/src/main/kotlin/com/example/riverpod_todoapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.riverpod_todoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/on_boarding/views/on_boarding_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// OnboardingScreen
+  // Imaga
+  // Title
+  // Sub Title
+  // Skip Button
+  // Swipe Indicator
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
