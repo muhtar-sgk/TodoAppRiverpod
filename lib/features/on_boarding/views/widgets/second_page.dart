8d44eec9879f40f7bc45f77bcf5240142519d26b
muhtar-sgk <muhtarrn@gmail.com>
2023-07-26 19:24:03 +0700

    add button on page two

--- lib/features/on_boarding/views/widgets/second_page.dart
+++ lib/features/on_boarding/views/widgets/second_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:riverpod_todoapp/core/res/colours.dart';
 
 import '../../../../core/common/widgets/white_space.dart';
 import '../../../../core/res/image_res.dart';
@@ -8,12 +10,28 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         Image.asset(ImageRes.todo),
-        const WhiteSpace(height: 80),
-        
+        const WhiteSpace(height: 50),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colours.light,
+            minimumSize: Size(size.width * .9, size.height * .06)
+          ),
+          onPressed: (){}, 
+          child: Text(
+            'Login with phone',
+            style: GoogleFonts.poppins(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colours.darkBackground
+            ),
+          )
+        )
       ]),
     );
   }
