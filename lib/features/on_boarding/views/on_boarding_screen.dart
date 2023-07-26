7af0891bc3cb2a73bc04a6fb439bf23f1310ce35
muhtar-sgk <muhtarrn@gmail.com>
2023-07-26 13:00:14 +0700

    add sized box

--- lib/features/on_boarding/views/on_boarding_screen.dart
+++ lib/features/on_boarding/views/on_boarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:riverpod_todoapp/core/res/image_res.dart';
 
 // OnboardingScreen
@@ -19,11 +20,13 @@ class OnBoardingScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 30),
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             Image.asset(ImageRes.todo),
+            SizedBox(height: 100.h),
             const Text(
               'Todo with Riverpod',
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
             ),
+            SizedBox(height: 10.h),
             const Text(
               'Welcome in Todo with Riverpod, you can create and clear note super fast',
               textAlign: TextAlign.center,
--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
+      splitScreenMode: true,
       builder: (context, child) {
         return MaterialApp(
           title: 'Todo App',
