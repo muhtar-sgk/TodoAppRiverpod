95f43bd588211454199534e6c25ef7173646be3b
muhtar-sgk <muhtarrn@gmail.com>
2023-07-28 19:31:37 +0700

    add firebase setup

--- lib/features/authentication/views/otp_verification_screen.dart
+++ lib/features/authentication/views/otp_verification_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+import 'package:riverpod_todoapp/core/common/widgets/white_space.dart';
+import 'package:riverpod_todoapp/core/res/colours.dart';
+import 'package:riverpod_todoapp/core/res/image_res.dart';
 
 class OTPVerificationScreen extends StatelessWidget {
   const OTPVerificationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(ImageRes.todo),
+              const WhiteSpace(height: 26),
+              Pinput(
+                length: 6,
+                onCompleted: (value) {
+                  debugPrint(value);
+                },
+                defaultPinTheme: PinTheme(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 16
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colours.light,
+                    borderRadius: BorderRadius.circular(8)
+                  )
+                ),
+              )
+            ],
+          ),
+        )),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:riverpod_todoapp/core/res/colours.dart';
+import 'package:riverpod_todoapp/firebase_options.dart';
 
 import 'features/on_boarding/views/on_boarding_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
