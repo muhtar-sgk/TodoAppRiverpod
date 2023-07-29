1ef2c85fcd5dd1a0663c349f44314acd4a628a3b
muhtar-sgk <muhtarrn@gmail.com>
2023-07-29 14:22:32 +0700

    create send otp function

--- lib/features/authentication/repository/authentication_repository.dart
+++ lib/features/authentication/repository/authentication_repository.dart
@@ -1,3 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
 class AuthenticationRepository {
-  
+  final FirebaseAuth auth;
+
+  const AuthenticationRepository({
+    required this.auth
+  });
+
+  Future<void> sendOTP({
+    required BuildContext context,
+    required String phoneNumber
+  }) async {
+    await auth.verifyPhoneNumber(
+      phoneNumber: phoneNumber,
+      verificationCompleted: (credential){
+        auth.signInWithCredential(credential);
+      }, 
+      verificationFailed: (exception){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('${exception.code}: ${exception.message}')
+          )
+        );
+      }, 
+      codeSent: (verificationId, _){}, 
+      codeAutoRetrievalTimeout: (verificationId){}
+    );
+  }
 }
