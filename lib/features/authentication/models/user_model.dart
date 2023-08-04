c1bbc2eb7f6fdc874aff3f381fa1982bb71cdc9d
muhtar-sgk <muhtarrn@gmail.com>
2023-08-04 16:31:21 +0700

    update user model

--- lib/features/authentication/models/user_model.dart
+++ lib/features/authentication/models/user_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class UserModel {
   final int id;
   final bool isVerified;
@@ -6,4 +8,18 @@ class UserModel {
     required this.id,
     required this.isVerified
   });
+
+  factory UserModel.fromJson(String source) => 
+    UserModel.fromMap(jsonDecode(source) as Map<String, dynamic>);
+  
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] as int, 
+      isVerified: map['isVerified'] as bool
+    );
+  }
+
+  Map<String, dynamic> toMap() => {'id': id, 'isVerified': isVerified};
+
+  String toJson() => jsonEncode(toMap());
 }
