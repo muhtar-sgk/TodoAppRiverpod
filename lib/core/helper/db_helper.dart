96a8456eeef95606efa32979e3e7d535285b92e3
muhtar-sgk <muhtarrn@gmail.com>
2023-08-04 17:13:37 +0700

    update database helper

--- lib/core/helper/db_helper.dart
+++ lib/core/helper/db_helper.dart
@@ -1,9 +1,38 @@
-import 'package:sqflite/sqlite_api.dart';
+import 'package:sqflite/sqflite.dart';
 
 class DBHelper {
   const DBHelper._();
 
   static Future<void> createTables(Database database) async {
-    
+    await database.execute('CREATE TABLE users ('
+      'id INTEGER PRIMARY KEY AUTOINCREMENT DEFAULT 0, '
+      'isVerified INTEGER DEFAULT 0'
+      ')'
+    );
+  }
+
+  static Future<Database> db() async {
+    return openDatabase(
+      'todo.db',
+      version: 1,
+      onCreate: (database, _) async {
+        await createTables(database);
+      }
+    );
+  }
+
+  static Future<void> createUser({required bool isVerified}) async {
+    final localDb = await db();
+
+    final data = {
+      'id': 1,
+      'isVerified': isVerified ? 1 : 0
+    };
+
+    await localDb.insert(
+      'users', 
+      data,
+      conflictAlgorithm: ConflictAlgorithm.replace
+    );
   }
 }
