10766e2175e14dc6339d74e99f24ad51b7fb585b
muhtar-sgk <muhtarrn@gmail.com>
2023-08-04 19:09:46 +0700

    add home screen

--- lib/features/todo/views/home_screen.dart
+++ lib/features/todo/views/home_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(85),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const RotatedBox(
+                    quarterTurns: 2,
+                    child: Icon(AntDesign.logout),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
